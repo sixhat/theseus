0eb62ee4fc4a65bb42736b11bd758331218f6a21
Dave <sixhat@users.noreply.github.com>
2024-07-09 18:13:38 +0100

    Removed wget Dependency

--- crawler/getUrl.py
+++ crawler/getUrl.py
@@ -7,7 +7,7 @@ getUrl.py
 Parses a downloaded RSS file and downloads all items in the Feed
 
 Usage:
-    getUrl.py <folder> <timeout>
+    getUrl.py <folder> <timeout> <rss_url>
 
     folder is the folder where the rss.xml file was placed.
     timeout is in seconds
@@ -19,59 +19,71 @@ import feedparser
 import time
 import zlib
 import requests
+from typing import List
 
 
-def main():
+def load_urls(folder_path: str) -> List[str]:
+    try:
+        with open(f"{folder_path}/urls.txt", 'r') as file:
+            return [line.split()[0] for line in file]
+    except FileNotFoundError:
+        return []
+
+
+def write_url(item: str, folder_path: str, ) -> None:
+    with open(f"{folder_path}/urls.txt", 'a') as file:
+        file.write(item)
+
+
+def generate_filename(item) -> str:
+    try:
+        updated = item.updated_parsed
+        timestamp = f"{updated[0]:04d}-{updated[1]:02d}-{updated[2]:02d}_{updated[3]:02d}h{updated[4]:02d}m{updated[6]:02d}s"
+    except Exception as e:
+        print(f"Exception when parsing timestamp: {e}")
+        timestamp = time.strftime("%Y-%j_%H-%M-%S", time.gmtime())
+
+    cifra = str(zlib.adler32(item.link.encode()))
+    return f"{timestamp}_{cifra}.html"
+
+
+def download_item(item, folder_path: str) -> None:
+    r = requests.get(item.link)
+    if r.status_code == 200:
+        filename = generate_filename(item)
+        with open(os.path.join(folder_path, filename), 'w', encoding='utf-8') as out:
+            out.write(r.text)
+    else:
+        with open(os.path.join(folder_path, "notfound.txt"), 'a') as out:
+            out.write(f"{item.link}\n")
+
+def download_rss(rss_url, folder_path: str) -> None:
+    r = requests.get(rss_url)
+    if r.status_code == 200:
+        with open(os.path.join(folder_path, "rss.xml"), 'w') as out:
+            out.write(r.text)
+    else:
+        print("Error, no RSS to download")
+
+def main(folder_path, timeout, rss_url):
     """
     This module receives as argument the folder where the rss.xml file is stored
     """
-    # Parse the downloaded rss.xml file
+    download_rss(rss_url, folder_path)
     rss = feedparser.parse(str(sys.argv[1]) + "/rss.xml")
-    urls = []  # The already downloaded list of URLS
-    # Let's see if the downloaded list is present and construct the urls list
-    try:
-        urls_file = open(sys.argv[1] + '/urls.txt', 'r+')
-        for line in urls_file:
-            urls.append(line.split()[0])
-    except Exception:
-        urls_file = open(sys.argv[1] + '/urls.txt', 'w')
+    urls = load_urls(folder_path)
 
     # Let's iterate over all entries in the parsed RSS
     for item in rss.entries:
-        if len(item.link) > 7:
-            try:
-                nome = str(item.updated_parsed[0]) + "-" + str(item.updated_parsed[1]) + "-" + str(
-                    item.updated_parsed[2]) + "_"
-                nome = nome + str(item.updated_parsed[3]) + "h" + str(item.updated_parsed[4]) + "m" + str(
-                    item.updated_parsed[6]) + "s_"
-            except Exception as e:
-                print(f"Exception {e}")
-                nome = time.strftime("%Y-%j_%H-%M-%S", time.gmtime())
-
-            cifra = str(zlib.adler32(item.link.encode()))
-            nome = nome + "_" + cifra + ".html"  # let's set the name of the output file based on the time
-
-            if str(item.link).encode("utf-8") not in urls:
-
-                r = requests.get(item.link)
-                if r.status_code == 200:
-                    out = open(sys.argv[1] + "/" + nome, 'w')
-                    out.write(r.text)
-                    out.close()
-                else:
-                    out2 = open(sys.argv[1] + "/" + "notfound.txt", 'a')
-                    out2.write(str(item.link).encode("utf-8") + "\n")
-                    out2.close()
-
-                urls_file.write(item.link+ "\n")  # Add this item to the downloaded URLS file
-                urls_file.flush()  # Let's flush the internal buffer
-                os.fsync(urls_file.fileno())  # Let's force write of all internal buffers to disk
-                time.sleep(int(sys.argv[2]))  # Wait TIMEOUT seconds to avoid DDoS blacklistings...
-    urls_file.close()
+        if len(item.link) > 7 and item.link not in urls:
+            download_item(item, folder_path)
+            write_url(item.link + "\n", folder_path)
+            urls.append(item.link)
+            time.sleep(timeout)  # Wait TIMEOUT seconds to avoid DDoS blacklistings...
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        main()
+    if len(sys.argv) == 4:
+        main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
     else:
         print(__doc__)
